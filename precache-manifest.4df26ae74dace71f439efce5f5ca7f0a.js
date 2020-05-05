bd931cae0930f50edae25a7e346ea2e75165861e
PhatOrbs <eddcollado@gmail.com>
2020-05-05 10:03:47 -0600

    Updates

---
+++ precache-manifest.4df26ae74dace71f439efce5f5ca7f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1aa55e0443d116b8890b6fb15f08d9d",
+    "url": "/Shenron-Software/index.html"
+  },
+  {
+    "revision": "a056ac720e6f8f20da78",
+    "url": "/Shenron-Software/static/css/main.5ce1bf8b.chunk.css"
+  },
+  {
+    "revision": "d6b789169de5aef34be6",
+    "url": "/Shenron-Software/static/js/2.e3f36e4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Shenron-Software/static/js/2.e3f36e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a056ac720e6f8f20da78",
+    "url": "/Shenron-Software/static/js/main.33f0864a.chunk.js"
+  },
+  {
+    "revision": "277e949b8699e342b712",
+    "url": "/Shenron-Software/static/js/runtime-main.7fb811c1.js"
+  }
+]);
