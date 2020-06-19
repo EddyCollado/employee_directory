22e7a0805287fa79082f00c3944ea28670780e53
Eddy Collado <eddcollado@gmail.com>
2020-06-19 12:21:18 -0600

    removed old files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import logo from './logo.svg';
-// import './App.css';
 import DataArea from "./components/DataArea/DataArea";
 import Footer from "./components/Footer/index";
-// import Container from "./components/Container";
 import Header from "./components/Header/Header";
-// import Search from "./pages/Search";
 import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-
-    //     <p>
-    //       Testing, react, I AM HERE!
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <Router>
       <div>
         <Wrapper>
@@ -43,5 +19,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;
